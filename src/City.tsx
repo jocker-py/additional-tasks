73fc86651036b665b6a1c06507463721315c3f42
jocker-py <buladzislau@gmail.com>
2023-04-05 20:24:34 +0300

    refactor: remove commentsa

--- src/City.tsx
+++ src/City.tsx
@@ -1,39 +1,22 @@
-import React from 'react';
+import React, {FC} from "react";
 import {CurrentBankomat} from "./CurrentBankomat";
 import {MoneyType} from "./App";
 
 type CityPropsType = {
-    data: Array<MoneyType> //встречаем денюжки
+  data: Array<MoneyType>
 }
 
-export const City = (props: CityPropsType) => {
+export const City: FC<CityPropsType> = ({data}) => {
+  const mappedMoney = data.map((el: MoneyType, index) => (
+    <CurrentBankomat key={index} money={el}/>
+  ));
 
-    // пока это пропускам
-    // const mappedMoney = props.data.map((el: MoneyType, index) => (
-    //     <CurrentBankomat
-    //         key={index}
-    //         money={el}
-    //     />
-    // ))
-
-
-    return (
-        <div>
-            <div>Ну все парни, мапимся -выводим наши денюжки</div>
-            <div>На верстку ПОКА не обращаем внимания , сейчас занимаемся логикой</div>
-            {/*<div>{mappedMoney}</div>*/}
-        </div>
-    );
+  return (
+    <div>
+      <div>{mappedMoney}</div>
+    </div>
+  );
 };
-//1
-// Вроде все норм, но нужно причесать код. Давайте создадим const mappedMoney = props.data.map(el=>el...)
-// Т.е. нам нужно вынести map из вертски, оставив в верстке только mappedMoney
-
-//2
-// Отлично, но теперь давайте совсем все сделаем по красоте. Вынесем верстку в отдельную компоненту и ее замапим!
-// Ну и наконец давайте версточку натянем, для этого будем использовать StyledComponents
-// yarn add styled-components
-// yarn add @types/styled-components
 
 //3
 // Вроде все норм, ну точнее почти норм- дублирование-это грех. Хотелось бы от него избавиться.
