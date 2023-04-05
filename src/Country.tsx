2d0453fa7655ced52067f215ddd0330008843333
jocker-py <buladzislau@gmail.com>
2023-04-05 17:57:00 +0300

    refactor: refactor spaces and quotes

--- src/Country.tsx
+++ src/Country.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
+import React from "react";
 import {City} from "./City";
 import {BanknotsType, MoneyType} from "./App";
 
 type CountryPropsType = {
-    data: Array<MoneyType>
-    setFilterValue: (filter: BanknotsType) => void
-  }
+  data: Array<MoneyType>
+  setFilterValue: (filter: BanknotsType) => void
+}
 
 export const Country = ({data, setFilterValue}: CountryPropsType) => {
-    const setAll = () => setFilterValue('All');
-    const setDollars = () => setFilterValue('Dollars');
-    const setRUBLS = () => setFilterValue('RUBLS');
+  const setAll = () => setFilterValue("All");
+  const setDollars = () => setFilterValue("Dollars");
+  const setRUBLS = () => setFilterValue("RUBLS");
 
-    return (
-        <div>
-            <button onClick={()=>{'засетаем All'}}>All</button>
-            <button onClick={()=>{'засетаем Dollars'}}>Dollars</button>
-            <button onClick={()=>{'засетаем RUBLS'}}>RUBLS</button>
-            <City data={"передаем денюжки в город"}/>
-        </div>
-    );
+  return (
+    <div>
+      <button onClick={() => {
+        "засетаем All";
+      }}>All
+      </button>
+      <button onClick={() => {
+        "засетаем Dollars";
+      }}>Dollars
+      </button>
+      <button onClick={() => {
+        "засетаем RUBLS";
+      }}>RUBLS
+      </button>
+      <City data={"передаем денюжки в город"}/>
+    </div>
+  );
 };
 
