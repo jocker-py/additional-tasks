ec19598ac9c98ecd42e82f4543e06fc1d936fcda
jocker-py <buladzislau@gmail.com>
2023-04-05 17:50:03 +0300

    refactor: refactor spaces and quotes

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,45 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
 import {Country} from "./Country";
 
-export type BanknotsType = 'Dollars' | 'RUBLS' | 'All';
+export type BanknotsType = "Dollars" | "RUBLS" | "All";
 export type MoneyType = {
-    banknotes: BanknotsType
-    value: number
-    number: string
+  banknotes: BanknotsType
+  value: number
+  number: string
 }
 
 let defaultMoney: Array<MoneyType> = [
-    {banknotes: 'Dollars', value: 100, number: ' a1234567890'},
-    {banknotes: 'Dollars', value: 50, number: ' z1234567890'},
-    {banknotes: 'RUBLS', value: 100, number: ' w1234567890'},
-    {banknotes: 'Dollars', value: 100, number: ' e1234567890'},
-    {banknotes: 'Dollars', value: 50, number: ' c1234567890'},
-    {banknotes: 'RUBLS', value: 100, number: ' r1234567890'},
-    {banknotes: 'Dollars', value: 50, number: ' x1234567890'},
-    {banknotes: 'RUBLS', value: 50, number: ' v1234567890'},
-]
+  {banknotes: "Dollars", value: 100, number: " a1234567890"},
+  {banknotes: "Dollars", value: 50, number: " z1234567890"},
+  {banknotes: "RUBLS", value: 100, number: " w1234567890"},
+  {banknotes: "Dollars", value: 100, number: " e1234567890"},
+  {banknotes: "Dollars", value: 50, number: " c1234567890"},
+  {banknotes: "RUBLS", value: 100, number: " r1234567890"},
+  {banknotes: "Dollars", value: 50, number: " x1234567890"},
+  {banknotes: "RUBLS", value: 50, number: " v1234567890"},
+];
 
 export const moneyFilter = (money: Array<MoneyType>, filter: BanknotsType): Array<MoneyType> => {
-    return [...money].filter(item => filter === 'All' ? true : filter === item.banknotes);
-}
+  return [...money].filter(item => filter === "All" ? true : filter === item.banknotes);
+};
 
 function App() {
-    const [money, setMoney] = useState<Array<MoneyType>>([])
-    const [filterValue, setFilterValue] = useState<any>('')
+  const [money, setMoney] = useState<Array<MoneyType>>([]);
+  const [filterValue, setFilterValue] = useState<any>("");
 
-    // а вот сейчас притормаживаем. И вдумчиво: константа filteredMoney получает результат функции moneyFilter
-    // в функцию передаем деньги и фильтр, по которому ихбудем выдавать(ретёрнуть)
-    const filteredMoney = moneyFilter(грошы, фильтръ)
-    return (
-        <div className="App">
-            <Country
-                data={filteredMoney}   //отрисовать будем деньги после фильтрации
-                setFilterValue={setFilterValue}  //useState передаем? Так можно было?!
+  // а вот сейчас притормаживаем. И вдумчиво: константа filteredMoney получает результат функции moneyFilter
+  // в функцию передаем деньги и фильтр, по которому ихбудем выдавать(ретёрнуть)
+  const filteredMoney = moneyFilter(грошы, фильтръ);
+  return (
+    <div className="App">
+      <Country
+        data={filteredMoney}   //отрисовать будем деньги после фильтрации
+        setFilterValue={setFilterValue}  //useState передаем? Так можно было?!
 
-            />
-        </div>
-    );
+      />
+    </div>
+  );
 }
 
 // Итого: в этой компоненте у нас мозги. А вот отрисовка где-то глубже. Погружаемся в Country
